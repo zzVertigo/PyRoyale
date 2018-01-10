207f914c577f7be8363f434b1fe5b631906f8d0b
Vertigo <alt.gk-1mh10h2@yopmail.com>
2018-01-09 22:20:37 -0800

    Update Login.py

--- src/Packets/Messages/Client/Login.py
+++ src/Packets/Messages/Client/Login.py
@@ -10,8 +10,23 @@ class Login(ByteStream):
         self.device = device
 
     def decode(self):
-        self.ReadUint32()
-        # Todo: Read Stuff
+        print("HighID: {}".format(self.ReadUint32()))
+        print("LowID: {}".format(self.ReadUint32()))
+        print("Token: {}".format(self.ReadString()))
+        print("Major: {}".format(self.ReadVint()))
+        print("Minor: {}".format(self.ReadVint()))
+        print("Revision: {}".format(self.ReadVint()))
+        print("Masterhash: {}".format(self.ReadString()))
+        print("Unknown: {}".format(self.ReadUint32()))
+        print("AndroidID: {}".format(self.ReadString()))
+        print("Unknown: {}".format(self.ReadString()))
+        print("Device Model: {}".format(self.ReadString()))
+        print("OpenUDID: {}".format(self.ReadString()))
+        print("OS Version: {}".format(self.ReadString()))
+        print("IsAndroid: {}".format(self.ReadBool()))
+        print("Unknown: {}".format(self.ReadUint32()))
+        print("AndroidID: {}".format(self.ReadString()))
+        print("Language: {}".format(self.ReadString()))
 
     def process(self):
         LoginOk(self.device).Send()
